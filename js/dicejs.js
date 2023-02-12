9b2b7b66f0fbafa5c06cf1532a95cda4ed57d5b0
mirza yousuf <myzbaig@mun.ca>
2023-02-12 20:05:59 -0330

    fav

--- js/dicejs.js
+++ js/dicejs.js
@@ -1,12 +1,12 @@
 
 
 var cubes= {
-    1:"C:/Users/yousu/Projects/Web/DiceGame/images/dice1.png",
-    2:"C:/Users/yousu/Projects/Web/DiceGame/images/dice2.png", 
-    3:"C:/Users/yousu/Projects/Web/DiceGame/images/dice3.png",
-    4:"C:/Users/yousu/Projects/Web/DiceGame/images/dice4.png",
-    5:"C:/Users/yousu/Projects/Web/DiceGame/images/dice5.png",
-    6:"C:/Users/yousu/Projects/Web/DiceGame/images/dice6.png"
+    1:"images/dice1.png",
+    2:"images/dice2.png", 
+    3:"images/dice3.png",
+    4:"images/dice4.png",
+    5:"images/dice5.png",
+    6:"images/dice6.png"
 }
 
 
